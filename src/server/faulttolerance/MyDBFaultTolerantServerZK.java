925afacd15187f7ae93f8a902dbb32f172d399e1
Maneesha Tejani <maneeshatejani@gmail.com>
2023-12-01 00:34:13 -0500

    first commit

--- src/server/faulttolerance/MyDBFaultTolerantServerZK.java
+++ src/server/faulttolerance/MyDBFaultTolerantServerZK.java
@@ -1,14 +1,26 @@
 package server.faulttolerance;
 
+import edu.umass.cs.nio.AbstractBytePacketDemultiplexer;
+import edu.umass.cs.nio.MessageNIOTransport;
 import edu.umass.cs.nio.interfaces.NodeConfig;
 import edu.umass.cs.nio.nioutils.NIOHeader;
 import edu.umass.cs.nio.nioutils.NodeConfigUtils;
 import edu.umass.cs.utils.Util;
-import server.AVDBReplicatedServer;
+import server.MyDBReplicatedServer;
 import server.ReplicatedServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.apache.zookeeper.*;
+import org.json.JSONObject;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 
 /**
  * This class should implement your replicated fault-tolerant database server if
@@ -16,7 +28,7 @@ import java.net.InetSocketAddress;
  * requests.
  * <p>
  * Refer to {@link server.ReplicatedServer} for a starting point for how to do
- * server-server messaging or to {@link server.AVDBReplicatedServer} for a
+ * server-server messaging or to {@link server.MyDBReplicatedServer} for a
  * non-fault-tolerant replicated server.
  * <p>
  * You can assume that a single *fault-tolerant* Zookeeper server at the default
@@ -29,7 +41,7 @@ import java.net.InetSocketAddress;
  * You can not store in-memory information about request logs for more than
  * {@link #MAX_LOG_SIZE} requests.
  */
-public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
+public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer implements Watcher{
 
 	/**
 	 * Set this value to as small a value with which you can get tests to still
@@ -55,6 +67,40 @@ public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
 	public static final int MAX_LOG_SIZE = 400;
 
 	public static final int DEFAULT_PORT = 2181;
+    private static final String ZK_HOST = "localhost";
+    private static final String ZK_ELECTION_PATH = "/election";
+    private static final String ZK_SERVICE_PATH = "/service";
+    private ZooKeeper zk;
+
+    final private Session session;
+    final private Cluster cluster;
+
+    protected final String myID;
+    protected final MessageNIOTransport<String,String> serverMessenger;
+
+    protected String leader;
+    
+    // this is the message queue used to track which messages have not been sent yet
+    private ConcurrentHashMap<Long, JSONObject> queue = new ConcurrentHashMap<Long, JSONObject>();
+    private CopyOnWriteArrayList<String> notAcked;
+    
+    // the sequencer to track the most recent request in the queue
+    private static long reqnum = 0;
+    synchronized static Long incrReqNum() {
+    	return reqnum++;
+    }
+    
+    // the sequencer to track the next request to be sent
+    private static long expected = 0;
+    synchronized static Long incrExpected() {
+    	return expected++;
+    }
+
+    protected static enum Type {
+		REQUEST, // a server forwards a REQUEST to the leader
+		PROPOSAL, // the leader broadcast the REQUEST to all the nodes
+        ACKNOWLEDGEMENT; // all the nodes send back acknowledgement to the leader
+	}
 
 	/**
 	 * @param nodeConfig Server name/address configuration information read
@@ -72,9 +118,85 @@ public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
 		super(new InetSocketAddress(nodeConfig.getNodeAddress(myID),
 				nodeConfig.getNodePort(myID) - ReplicatedServer
 						.SERVER_PORT_OFFSET), isaDB, myID);
+        session = (cluster=Cluster.builder().addContactPoint("127.0.0.1")
+                .build()).connect(myID);
+        
+        this.myID = myID;
+
+        this.serverMessenger =  new
+                MessageNIOTransport<String, String>(myID, nodeConfig,
+                new
+                        AbstractBytePacketDemultiplexer() {
+                            @Override
+                            public boolean handleMessage(byte[] bytes, NIOHeader nioHeader) {
+                                handleMessageFromServer(bytes, nioHeader);
+                                return true;
+                            }
+                        }, true);
+                    
+        // Connect to Zookeeper server
+        try {
+            this.zk = new ZooKeeper(ZK_HOST + ":" + DEFAULT_PORT, 3000, this);
+
+            // Create a znode for the replica
+            zk.create(ZK_ELECTION_PATH + "/" + this.myID, this.myID.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            zk.create(ZK_SERVICE_PATH + "/" + this.myID, this.myID.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+            // Set a watch on the replicas znode to monitor changes
+            zk.exists(ZK_ELECTION_PATH, true);
+
+            // elect a leader using cassandra's znodes 
+            electLeader(); 
+        } catch (KeeperException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
 
 		// TODO: Make sure to do any needed crash recovery here.
 	}
+
+    private void electLeader() {
+        try {
+            // check if leader is gone
+            // if leader gone, elect
+            List<String> children = zk.getChildren(ZK_ELECTION_PATH, true);
+            if (children.contains(this.leader)){
+                
+            }
+
+            this.leader = Collections.max(children);
+
+            if (this.myID == this.leader){
+                // add leader znode
+            }
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        // init msg
+        switch (event.getType()) {
+            case None:
+                // SyncConnected
+                break;
+        
+            case NodeChildrenChanged:
+                electLeader();
+                break;
+            
+            case NodeDeleted:
+                if(event.getPath() == ZK_ELECTION_PATH + "/" + this.leader){
+                    electLeader();
+                }
+            default:
+                break;
+        }
+    }
+
+    public void crashRecovery(WatchedEvent event){
+        
+    }
 
 	/**
 	 * TODO: process bytes received from clients here.
@@ -95,7 +217,10 @@ public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
 	 * TODO: Gracefully close any threads or messengers you created.
 	 */
 	public void close() {
-		throw new RuntimeException("Not implemented");
+        super.close();
+	    this.serverMessenger.stop();
+	    session.close();
+	    cluster.close();
 	}
 
 	public static enum CheckpointRecovery {
